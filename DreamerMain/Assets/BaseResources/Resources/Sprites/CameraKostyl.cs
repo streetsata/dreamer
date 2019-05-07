e9f3f98828cf0091b9de8364b806604381d421ad
metelindv <dmitrij.metelin@murka.com>
2019-05-07 18:29:57 +0300

    [*] add demo logic

---
+++ DreamerMain/Assets/BaseResources/Resources/Sprites/CameraKostyl.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraKostyl : MonoBehaviour {
+
+    public Transform papa;
+    public float speed;
+
+	// Update is called once per frame
+	void FixedUpdate () {
+        Vector3 a = transform.position;
+        transform.position = Vector3.Lerp(a, papa.position, speed);
+	}
+}
---
+++ DreamerMain/Assets/BaseResources/Resources/Sprites/ForDemooo.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForDemooo : MonoBehaviour {
+
+    public GameObject g;
+    public float endDeadLine = -10000f;
+	
+	// Update is called once per frame
+	void Update () {
+        if (transform.position.y <= endDeadLine)
+        {
+            g.SetActive(true);
+            Application.LoadLevel("demooooo");
+        }
+	}
+}
